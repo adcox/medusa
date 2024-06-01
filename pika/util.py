4f846e0f00ddcaf3f1e2e5606d7d7c9212ff87e1
Andrew Cox <andrewdcox10@gmail.com>
2024-05-31 19:36:19 -0700

    add util module

---
+++ pika/util.py
@@ -0,0 +1,25 @@
+"""
+Utilities
+"""
+
+
+def _iterate(val):
+    # Iterate on an object
+    if isinstance(val, str):
+        yield val
+    else:
+        try:
+            for item in val:
+                yield item
+        except TypeError:
+            yield val
+
+
+def toList(val):
+    # Convert to list
+    return list(_iterate(val))
+
+
+def toArray(val):
+    # Convert to array
+    return np.asarray(_iterate(val))
