fix bug in numerical derivatives; resolves #21

pika/numerics.py
@@ -36,7 +36,6 @@ def derivative(func, x, step, nIter=10, maxRelChange=2.0, meta={}):
     CON2 = CON * CON
 
     # Generalize to arbitrary dimensions
-    isMultiVar = not isinstance(x, float)
     x = np.asarray(x)
     step = np.asarray(step)
     stepSz = np.linalg.norm(step)
@@ -89,7 +88,7 @@ def derivative(func, x, step, nIter=10, maxRelChange=2.0, meta={}):
 
     # Save metadata and return best derivative approximation
     meta.update({"err": err, "count": col, "tableau": tableau})
-    return deriv if isMultiVar else deriv.flat[0]
+    return deriv
 
 
 def derivative_multivar(func, x, step, nIter=10, maxRelChange=2.0):
@@ -118,7 +117,9 @@ def derivative_multivar(func, x, step, nIter=10, maxRelChange=2.0):
     for ix in range(x.size):
         pert = np.zeros(x.shape)
         pert[ix] = step[ix]
-        deriv[:, ix] = derivative(func, x, pert, nIter=nIter, maxRelChange=maxRelChange)
+        deriv[:, ix] = np.squeeze(
+            derivative(func, x, pert, nIter=nIter, maxRelChange=maxRelChange)
+        )
 
     # TODO can we report out the metadata for each variable?
     return deriv

tests/test_lowthrust.py
@@ -121,7 +121,7 @@ class TestControlTerm:
         # TODO zero if epoch idependent
 
         # Check partials
-        func = lambda x: np.asarray(term.evalTerm(x, *integArgs[1:])).flatten()
+        func = lambda x: term.evalTerm(x, *integArgs[1:])
         numPartials = numerics.derivative(func, integArgs[0], 1e-4)
         np.testing.assert_allclose(partials, numPartials, atol=1e-8)
 
@@ -280,7 +280,7 @@ class TestForceMassOrientLaw_noStates:
         assert partials.shape == (3, 1)
 
         # Check partials
-        func = lambda x: np.asarray(law.accelVec(x, *integArgs[1:])).flatten()
+        func = lambda x: law.accelVec(x, *integArgs[1:])
         numPartials = numerics.derivative(func, integArgs[0], 1e-4)
         np.testing.assert_allclose(partials, numPartials, atol=1e-8)
 

tests/test_numerics.py
@@ -8,8 +8,8 @@ from pika import numerics
 
 
 @pytest.mark.parametrize("val", [0.0, 1.0, np.pi, np.pi / 2, 5.123])
-def test_derivative_sin(val):
-    # Test derivative computation with simple sin function
+def test_derivative_univar_scalar(val):
+    # univariate function, scalar output
     meta = {}
     deriv = numerics.derivative(np.sin, val, 0.25, nIter=11, meta=meta)
     assert isinstance(deriv, float)
@@ -28,12 +28,56 @@ def test_derivative_sin(val):
     assert meta["tableau"].shape == (11, 11)
 
 
+@pytest.mark.parametrize("val", [0.0, 1.0, np.pi, np.pi / 2, 5.123])
+def test_derivative_univar_rowVec(val):
+    # univariate function, row-vector output
+    func = lambda x: np.asarray([np.sin(x), np.cos(x)])
+    trueDeriv = np.asarray([np.cos(val), -np.sin(val)])
+
+    deriv = numerics.derivative(func, val, 0.25)
+    assert isinstance(deriv, np.ndarray)
+    np.testing.assert_allclose(deriv, trueDeriv, rtol=1e-4, atol=1e-8)
+
+
+@pytest.mark.parametrize("val", [0.0, 1.0, np.pi, np.pi / 2, 5.123])
+def test_derivative_univar_colVec(val):
+    # univariate function, column-vector output
+    func = lambda x: np.asarray([[np.sin(x)], [np.cos(x)]])
+    trueDeriv = np.asarray([[np.cos(val)], [-np.sin(val)]])
+
+    deriv = numerics.derivative(func, val, 0.25)
+    assert isinstance(deriv, np.ndarray)
+    np.testing.assert_allclose(deriv, trueDeriv, rtol=1e-4, atol=1e-8)
+
+
 @pytest.mark.parametrize("val", [[0.0, 0.0], [0.0, 1.0], [1.0, 0.0], [1.2, 5.4]])
-def test_derivative_multivar(val):
-    func = lambda x: np.asarray([np.sin(x[0]), np.cos(x[1])])
-    trueDeriv = lambda x: np.asarray([[np.cos(x[0]), 0.0], [0.0, -np.sin(x[1])]])
+def test_derivative_multivar_scalar(val):
+    # multivariate function, scalar output
+    func = lambda x: np.sin(x[0]) + np.cos(x[1])
+    trueDeriv = np.array([np.cos(val[0]), -np.sin(val[1])], ndmin=2)
 
     deriv = numerics.derivative_multivar(func, val, 0.25)
     assert isinstance(deriv, np.ndarray)
-    assert deriv.shape == (2, 2)
-    np.testing.assert_allclose(deriv, trueDeriv(val))
+    np.testing.assert_allclose(deriv, trueDeriv, rtol=1e-4, atol=1e-8)
+
+
+@pytest.mark.parametrize("val", [[0.0, 0.0], [0.0, 1.0], [1.0, 0.0], [1.2, 5.4]])
+def test_derivative_multivar_rowVec(val):
+    # multivariate function, row-vecor output
+    func = lambda x: np.asarray([np.sin(x[0]), np.cos(x[1])])
+    trueDeriv = np.asarray([[np.cos(val[0]), 0.0], [0.0, -np.sin(val[1])]])
+
+    deriv = numerics.derivative_multivar(func, val, 0.25)
+    assert isinstance(deriv, np.ndarray)
+    np.testing.assert_allclose(deriv, trueDeriv)
+
+
+@pytest.mark.parametrize("val", [[0.0, 0.0], [0.0, 1.0], [1.0, 0.0], [1.2, 5.4]])
+def test_derivative_multivar_colVec(val):
+    # multivariate function, column-vector output
+    func = lambda x: np.asarray([[np.sin(x[0])], [np.cos(x[1])]])
+    trueDeriv = np.asarray([[np.cos(val[0]), 0.0], [0.0, -np.sin(val[1])]])
+
+    deriv = numerics.derivative_multivar(func, val, 0.25)
+    assert isinstance(deriv, np.ndarray)
+    np.testing.assert_allclose(deriv, trueDeriv)